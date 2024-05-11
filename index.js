b46383684b2a3e3e8e486c7920730b2e50139543
gcdyhujdc <js1rasimiyi@hotmail.com>
2024-05-11 10:01:47 +0700

    feat project

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function isPalindrome(str) {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
 }
-
-console.log(isPalindrome("racecar")); // true
