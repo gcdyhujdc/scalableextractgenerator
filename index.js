b32b363b46267257fb99e5a3e83a59ffffab0e69
gcdyhujdc <js1rasimiyi@hotmail.com>
2024-05-12 22:43:00 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
+const countingSort = (arr) => {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const count = Array(max - min + 1).fill(0);
+  for (let num of arr) {
+    count[num - min]++;
   }
-  return false;
-}
+  let sortedIndex = 0;
+  for (let i = min; i <= max; i++) {
+    while (count[i - min] > 0) {
+      arr[sortedIndex++] = i;
+      count[i - min]--;
+    }
+  }
+  return arr;
+};
