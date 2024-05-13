d5d57dd5a3f8d6431ac55b213b032a22c2862dd7
gcdyhujdc <js1rasimiyi@hotmail.com>
2024-05-14 00:13:26 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const countingSort = (arr) => {
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const count = Array(max - min + 1).fill(0);
-  for (let num of arr) {
-    count[num - min]++;
+function maxProduct(nums) {
+  if (nums.length === 0) return 0;
+  let maxSoFar = nums[0];
+  let minSoFar = nums[0];
+  let maxProduct = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const temp = maxSoFar;
+    maxSoFar = Math.max(nums[i], nums[i] * maxSoFar, nums[i] * minSoFar);
+    minSoFar = Math.min(nums[i], nums[i] * temp, nums[i] * minSoFar);
+    maxProduct = Math.max(maxProduct, maxSoFar);
   }
-  let sortedIndex = 0;
-  for (let i = min; i <= max; i++) {
-    while (count[i - min] > 0) {
-      arr[sortedIndex++] = i;
-      count[i - min]--;
-    }
-  }
-  return arr;
-};
+  return maxProduct;
+}
